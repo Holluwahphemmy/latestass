1fd63d8b849234dee6a584fc13da88febddec4a5
Holluwahphemmy <amos.oluphemmy@gmail.com>
2025-04-23 11:05:22 +0100

    Update scripts.js

--- latestass/scripts.js
+++ latestass/scripts.js
@@ -7,21 +7,5 @@ for (let i = 0; i < newclass.length; i++) {
   if (element.innerText.includes("here")) {
     element.style.backgroundColor = "blue";
   }
-}
-let val = document.querySelector("ul");
-console.log(val.childNodes)
-console.log(val.children)
-console.log(val.firstChild)
-console.log(val.lastChild)
-
-
-
-let ul = document.createElement("ul");
-let li = document.createElement("li");
-let text = document.createTextNode("This is Femi");
-li.appendChild(text);
-ul.appendChild(li);
-
-document.body.appendChild(ul);
 
 
