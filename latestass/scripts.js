3e49eb7c36389cf25e43de6128a4c6eb14cc29bc
Holluwahphemmy <amos.oluphemmy@gmail.com>
2025-04-23 11:00:24 +0100

    Update scripts.js

--- latestass/scripts.js
+++ latestass/scripts.js
@@ -1,13 +1,13 @@
-// let newclass = document.getElementsByClassName("neww");
-// console.log(newclass.innerText);
-// console.log(newclass);
-// for (let i = 0; i < newclass.length; i++) {
-//   const element = newclass[i];
-//   console.log(element);
-//   if (element.innerText.includes("here")) {
-//     element.style.backgroundColor = "blue";
-//   }
-// }
+let newclass = document.getElementsByClassName("neww");
+console.log(newclass.innerText);
+console.log(newclass);
+for (let i = 0; i < newclass.length; i++) {
+  const element = newclass[i];
+  console.log(element);
+  if (element.innerText.includes("here")) {
+    element.style.backgroundColor = "blue";
+  }
+}
 let val = document.querySelector("ul");
 console.log(val.childNodes)
 console.log(val.children)
